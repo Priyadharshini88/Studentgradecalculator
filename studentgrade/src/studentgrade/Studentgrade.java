5d1626b4fccd741a4e4b235818ea4cedfa975372
Priyadharshini88 <144266507+Priyadharshini88@users.noreply.github.com>
2023-09-22 18:51:42 +0530

    Add files via upload

---
+++ studentgrade/src/studentgrade/Studentgrade.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package studentgrade;
+import java.io.*;
+import java.util.*;
+public class Studentgrade {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner s=new Scanner(System.in);
+        System.out.println("STUDENT GRADE CALCULATOR");
+        System.out.print("Enter no of subjects"+" ");
+        int nofsub=s.nextInt();
+        int total=0;
+        for(int i=1;i<=nofsub;i++){
+            System.out.print("Enter Mark "+i+" ");
+            int marks=s.nextInt();
+            total+=marks;
+        }
+        double average=(double)total/(nofsub*100)*100;
+       
+        System.out.println("your Average is "+String.format("%.2f", average));
+        char grade;
+        if(average>=90){
+            System.out.println("Your grade is A");
+        }else if(average>=80){
+            System.out.println("Your grade is B");
+        }else if(average>=70){
+            System.out.println("Your grade is C");
+        }else if(average>=60){
+            System.out.println("Your grade is D");
+        }else if(average>=50){
+            System.out.println("Your grade is E");
+        }
+        else {
+            System.out.println("Your grade is F");
+        }
+            }
+    
+}
